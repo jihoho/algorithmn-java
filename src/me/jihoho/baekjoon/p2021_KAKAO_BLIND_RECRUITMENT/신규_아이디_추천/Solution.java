a22a403e0cfca48402874e398933a2105542f8f6
jihoho <xkftn94@naver.com>
2021-06-22 00:22:04 +0900

    [2021 KAKAO BLIND RECRUITMENT] 신규 아이디 추천

---
+++ src/me/jihoho/baekjoon/p2021_KAKAO_BLIND_RECRUITMENT/신규_아이디_추천/Solution.java
@@ -0,0 +1,33 @@
+package me.jihoho.baekjoon.p2021_KAKAO_BLIND_RECRUITMENT.신규_아이디_추천;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: hojun
+ * Date: 2021-06-21 Time: 오후 10:57
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution("...!@BaT#*..y.abcdefghijklm"));
+    }
+
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9_.-]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+        if (answer.equals("")) {
+            answer = "a";
+        }
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("^[.]|[.]$", "");
+        }
+        while (answer.length() <= 2) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+        return answer;
+    }
+
+}
