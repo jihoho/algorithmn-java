1e5df59b04422e46f889f14296dd7988d6d395f9
jihoho <xkftn94@naver.com>
2021-07-02 19:14:28 +0900

    [월간 코드 챌린지 시즌2] 괄호 회전하기

---
+++ src/me/jihoho/baekjoon/p월간_코드_챌린지_시즌2/괄호_회전하기/Solution.java
@@ -0,0 +1,65 @@
+package me.jihoho.baekjoon.p월간_코드_챌린지_시즌2.괄호_회전하기;
+
+import java.util.Stack;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: hojun
+ * Date: 2021-07-02 Time: 오후 7:14
+ */
+public class Solution {
+
+    public int solution(String s) {
+        int answer = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String lotationStr = getLotationString(s, i);
+            if (isRightBrackets(lotationStr)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public String getLotationString(String s, int i) {
+        return s.substring(i, s.length()) + s.substring(0, i);
+    }
+
+    public boolean isRightBrackets(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (s.empty()) {
+                s.push(ch);
+            } else {
+                char top = s.peek();
+                switch (ch) {
+                    case ')':
+                        if (top == '(') {
+                            s.pop();
+                        }
+                        break;
+                    case ']':
+                        if (top == '[') {
+                            s.pop();
+                        }
+                        break;
+                    case '}':
+                        if (top == '{') {
+                            s.pop();
+                        }
+                        break;
+                    default:
+                        s.push(ch);
+                        break;
+                }
+            }
+        }
+
+        if (s.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
